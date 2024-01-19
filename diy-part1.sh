cf274b7918aad0c6323bf958b1e5fbd3b793ecdb
Stone <74167978+s71557@users.noreply.github.com>
2024-01-19 08:07:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,15 +24,6 @@
 
 # openclash
 svn export https://github.com/s71557/lede-packages/trunk/luci-app-openclash  package/luci-app-openclash
-# 加入OpenClash核心
-chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
-if [ "$1" = "rk33xx" ]; then
-    $GITHUB_WORKSPACE/preset-clash-core.sh arm64
-elif [ "$1" = "rk35xx" ]; then
-    $GITHUB_WORKSPACE/preset-clash-core.sh arm64
-elif [ "$1" = "x86" ]; then
-    $GITHUB_WORKSPACE/preset-clash-core.sh amd64
-fi
 
 # adguardhome
 svn export https://github.com/s71557/lede-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
